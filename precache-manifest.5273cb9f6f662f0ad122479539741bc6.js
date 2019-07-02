4d96353fad8ff4737dff7d6fd0db7c0e51d9dc74
Zachary McManus <zacharymcmanus@gmail.com>
2019-07-02 00:49:59 -0700

    Updates

---
+++ precache-manifest.5273cb9f6f662f0ad122479539741bc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bcb16c8cbbb24f2fe12",
+    "url": "/react-flashcards/static/css/main.c3b0942c.chunk.css"
+  },
+  {
+    "revision": "5bcb16c8cbbb24f2fe12",
+    "url": "/react-flashcards/static/js/main.1a6cae98.chunk.js"
+  },
+  {
+    "revision": "f21f1dcf700c93663418",
+    "url": "/react-flashcards/static/js/runtime~main.65a1b0a5.js"
+  },
+  {
+    "revision": "c2b8c506c6928d98960b",
+    "url": "/react-flashcards/static/js/2.2265941e.chunk.js"
+  },
+  {
+    "revision": "f6644c8e7327f7b9ffe3505439244613",
+    "url": "/react-flashcards/index.html"
+  }
+];
